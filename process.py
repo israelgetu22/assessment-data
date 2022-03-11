42691fdb9027887d6be07a3cf8d69708fda45b4b
Isarel Getu <israelgetu101@gmail.com>
2022-03-11 11:16:04 -0600

    Part 1 Done

--- process.py
+++ process.py
@@ -1,12 +1,26 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt")  #Open the file 'um-server-01.txe' using 'open("filename")'
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file):  #Creating a function in a name of 'sales_reports'
+    for line in log_file:     #loop or iteration of each log_file in variable line
+        line = line.rstrip()  #removes any characters at the end a string
+        day = line[0:3]       #slice from index 0 to index 3
+        if day == "Tue":      #condition for only Tuesday : 'Tue
+            print(line)       #output or console.log like js for variable 'line'
 
 
-sales_reports(log_file)
+sales_reports(log_file)       #calling the python function and invoke 'log_file'
+
+
+log_file = open("um-server-01.txt")  #Open the file 'um-server-01.txe' using 'open("filename")'
+
+
+def sales_reports(log_file):  #Creating a function in a name of 'sales_reports'
+    for line in log_file:     #loop or iteration of each log_file in variable line
+        line = line.rstrip()  #removes any characters at the end a string
+        day = line[0:3]       #slice from index 0 to index 3
+        if day == "Mon":      #condition for only Monday : 'Mon
+            print(line)       #output or console.log like js for variable 'line'
+
+
+sales_reports(log_file)        #calling the python function and invoke 'log_file'
